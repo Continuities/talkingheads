c09b337bda9fd0db8baaf05fc8a4563712c23591
Michael Townsend <michael@doublespeakgames.com>
2026-04-13 14:34:25 -0400

    more env

--- index.ts
+++ index.ts
@@ -5,13 +5,17 @@ import { SerialPort, ReadlineParser } from "serialport";
 if (!process.env.SERIAL_PATH) {
   throw "Environment SERIAL_PATH missing";
 }
+if (!process.env.FLUX_SCALE) {
+  throw "Environment FLUX_SCALE missing";
+}
+if (!process.env.ONSET_THRESHOLD) {
+  throw "Environment ONSET_THRESHOLD missing";
+}
 
 const CONSOLE_FPS = 30;
 const RMS_SMOOTHING = 0.3; // [0, 1] higher = smoother, but more lag; 0 = no smoothing, more jitter
 const SPEECH_PROB_THRESHOLD = 0.75; // [0, 1] threshold for speech detection
-const ONSET_THRESHOLD = 0.001; // rms delta for syllable detection. Tune to the mic.
 const MIN_ONSET_GAP_MS = 150; // minimum ms between onsets
-const FLUX_SCALE = 20; // how much flux (rms increase) maps to max strength
 const SERIAL_BAUD = 9600;
 
 const port = new SerialPort({
@@ -50,9 +54,9 @@ if (process.env.MICROPHONE_1_ID) {
     microphoneId: process.env.MICROPHONE_1_ID,
     rmsSmoothing: RMS_SMOOTHING,
     speechProbThreshold: SPEECH_PROB_THRESHOLD,
-    onsetThreshold: ONSET_THRESHOLD,
+    onsetThreshold: process.env.ONSET_THRESHOLD,
     minOnsetGapMs: MIN_ONSET_GAP_MS,
-    fluxScale: FLUX_SCALE,
+    fluxScale: process.env.FLUX_SCALE,
     setSolenoid: (open) => {
       sendSerial(`0:${open ? "1" : "0"}`);
     },
@@ -63,9 +67,9 @@ if (process.env.MICROPHONE_2_ID) {
     microphoneId: process.env.MICROPHONE_2_ID,
     rmsSmoothing: RMS_SMOOTHING,
     speechProbThreshold: SPEECH_PROB_THRESHOLD,
-    onsetThreshold: ONSET_THRESHOLD,
+    onsetThreshold: process.env.ONSET_THRESHOLD,
     minOnsetGapMs: MIN_ONSET_GAP_MS,
-    fluxScale: FLUX_SCALE,
+    fluxScale: process.env.FLUX_SCALE,
     setSolenoid: (open) => {
       sendSerial(`1:${open ? "1" : "0"}`);
     },
